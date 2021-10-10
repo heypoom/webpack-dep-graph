349968274a3525b21002ecc877cdcec385fd3509
Phoomparin Mano <poom@hey.com>
2021-10-10 16:48:33 +0700

    feat: cytoscape graph setup

--- packages/viewer/src/app.tsx
+++ packages/viewer/src/app.tsx
@@ -1,20 +1,72 @@
-import { Logo } from './logo'
+import type { ElementDefinition, Stylesheet } from "cytoscape"
+
+import CytoscapeComponent from "react-cytoscapejs"
+
+const node = (id: string): ElementDefinition => ({ data: { id } })
+
+const edge = (source: string, target: string): ElementDefinition => ({
+  data: { source, target },
+})
+
+const elements: ElementDefinition[] = [
+  node("A"),
+  node("B"),
+  node("C"),
+  edge("A", "B"),
+  edge("B", "C"),
+]
+
+const stylesheet: Stylesheet[] = [
+  {
+    selector: "node",
+    style: {
+      "background-color": "#111",
+      label: "data(id)",
+      color: "#b8e994",
+      "border-color": "#b8e994",
+      "border-width": 2,
+      "font-weight": 400,
+      "text-valign": "center",
+      "text-halign": "center",
+    },
+  },
+
+  {
+    selector: "edge",
+    style: {
+      width: 2,
+      "line-color": "#757575",
+      "target-arrow-color": "#757575",
+      "target-arrow-shape": "triangle",
+      "curve-style": "bezier",
+    },
+  },
+]
 
 export function App() {
   return (
-    <>
-      <Logo />
-      <p>Hello Vite + Preact!</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
-    </>
+    <div style={{ background: "#111" }}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300,400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
+      <CytoscapeComponent
+        layout={{ name: "cose" }}
+        stylesheet={stylesheet}
+        elements={elements}
+        style={{ width: "100vw", height: "100vh" }}
+      />
+    </div>
   )
 }
--- packages/viewer/src/main.tsx
+++ packages/viewer/src/main.tsx
@@ -1,5 +1,7 @@
-import { render } from 'preact'
-import { App } from './app'
-import './index.css'
+import { render } from "preact"
 
-render(<App />, document.getElementById('app')!)
+import { App } from "./app"
+
+import "./main.css"
+
+render(<App />, document.getElementById("app")!)
